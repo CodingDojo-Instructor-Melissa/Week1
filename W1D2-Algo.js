51024011ea2dcbf5cfeaed2a09a2687fdbc95a8f
Melissa Longenberger <melissalongenberger24@gmail.com>
2021-01-27 14:40:58 -0500

    initial commit

---
+++ W1D2-Algo.js
@@ -0,0 +1,23 @@
+// Understanding what is going on
+var x = [2,4,6]; // What does this say?
+console.log(x); // What will this do?
+
+
+// Index
+var y = [1,3,4]; 
+console.log(y[0]);
+
+// Using index to change the array
+var z = [5,6,9];
+z[0] = z[2]; // What is this saying?
+console.log(z); // What will be printed
+
+// .length
+
+var a = [6,24,50];
+var b = a.length; // What is this saying and doing?
+console.log(b); // What will be printed and why?
+
+var c = ["Hello", "There", "Houston"];
+var d = c.length;
+console.log(d);
